57d39ce6416e74d54171dcabac56809840822750
Shumail Mehboob <Shumail@Shumail-MacBook-Pro.local>
2025-04-22 13:44:04 +0500

    Simplify Next.js config for Vercel

---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"JDW/WTtJHUtZQCTjGRRLQJ0VdKmwbdHl1W9udbaPjIM=\"\n}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
